9c80140537ec54fef26eea14a1420cf2bfb64dfc
pketgto388 <pketgto388@gmail.com>
2023-04-02 11:01:11 +0800

    20230402

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -2,6 +2,27 @@ let addBtn = document.querySelector('#add-btn');
 let itemName = document.querySelector('#item-name');
 let item = document.querySelector('#item');
 
+item.addEventListener('click', (e) => {
+    let target = e.target;
+    let tag = target.tagName.toLowerCase();
+
+    // 因為結構上 input 與 div 上一層都是 li
+    if (tag == 'input' || tag == 'div') {
+        target = target.parentNode;
+        tag = target.tagName.toLowerCase();
+    }
+
+    // 當為 li 時表示處理該項目，過濾點到 UL
+    if (tag == 'li') {
+        let isDone = target.classList.contains('delete');
+        if (isDone) {
+            // 變成未完成
+        } else {
+            // 變成完成
+        }
+    }
+})
+
 const appendItem = (name) => {
     let li = document.createElement('li');
     let input = document.createElement('input');
